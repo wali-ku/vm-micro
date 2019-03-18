d4ed4c55ae8c13809d006fb2262250ef8c5a18fc
Waqar Ali <waqar.272@gmail.com>
2019-03-17 22:06:26 -0500

    profile: Execution data in DynamoRio
    Signed-off-by: Waqar Ali <waqar.272@gmail.com>

--- profile.sh
+++ profile.sh
@@ -5,7 +5,7 @@ jumps=(1 10 100 1000 10000 100000 1000000 10000000)
 for jump in ${jumps[@]}; do
 	for run in 1 2 3; do
 		echo "[STATUS] Run: ${run} | Jumps: ${jump}"
-		perf stat -o logs/j${jump}_r${run}.perf ./test ${jump}
+		perf stat -o logs/rio/j${jump}_r${run}.perf ../dynamorio/build/bin64/drrun ./test ${jump}
 	done
 	echo
 done
